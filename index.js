3e58bb91ea53eac703937e13b56fc57ddc504b5f
kennytran275 <kennyt275@gmail.com>
2021-02-18 17:33:57 -0500

    completed task 5

--- index.js
+++ index.js
@@ -100,8 +100,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
-
-
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
+console.log(reviews);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
